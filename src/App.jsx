768025765dee5a8da96864e339b06623321382ee
NhonVT <vothanhnhon104@gmail.com>
2023-08-29 14:33:38 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import { Area } from "@ant-design/plots";
 
 import "./App.css";
 
@@ -19,10 +19,11 @@ const data = [
         DATEMONTH: "2023-03-22T13:38:34.1015011+02:00",
         MO: 145.384615,
     },
+
     {
         MO_CODE: "CT2",
         DATEMONTH: "2023-01-22T13:38:34.1015011+02:00",
-        MO: 145.384615,
+        MO: 200,
     },
     {
         MO_CODE: "CT2",
@@ -34,21 +35,6 @@ const data = [
         DATEMONTH: "2023-07-22T13:38:34.1015011+02:00",
         MO: 145.384615,
     },
-    {
-        MO_CODE: "CT3",
-        DATEMONTH: "2023-01-22T13:38:34.1015011+02:00",
-        MO: 80.8,
-    },
-    {
-        MO_CODE: "CT3",
-        DATEMONTH: "2023-05-22T13:38:34.1015011+02:00",
-        MO: 60.89,
-    },
-    {
-        MO_CODE: "CT3",
-        DATEMONTH: "2023-07-22T13:38:34.1015011+02:00",
-        MO: 203.384615,
-    },
 ];
 
 function App() {
@@ -58,40 +44,17 @@ function App() {
         MO_CODE: item.MO_CODE,
     }));
 
-    const uniqueMoCodes = [...new Set(chartData.map((item) => item.MO_CODE))];
-
-    // Predefine colors for each unique MO_CODE
-    const moCodeColors = {
-        CT1: "#8884d8",
-        CT2: "#82ca9d",
-        CT3: "#f00",
-        CT4: "#000",
+    const config = {
+        data: chartData,
+        xField: "DATEMONTH",
+        yField: "MO",
+        seriesField: "MO_CODE",
     };
 
     return (
         <>
             <div className="box">
-                <ResponsiveContainer width="100%" height={600}>
-                    <AreaChart data={chartData}>
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="DATEMONTH" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-
-                        {uniqueMoCodes.map((code) => (
-                            <Area
-                                key={code}
-                                type="natural"
-                                dataKey="MO"
-                                stackId="1"
-                                stroke={moCodeColors[code]} // Use predefined color
-                                fill={moCodeColors[code]} // Use predefined color
-                                name={code}
-                            />
-                        ))}
-                    </AreaChart>
-                </ResponsiveContainer>
+                <Area {...config} />
             </div>
         </>
     );
